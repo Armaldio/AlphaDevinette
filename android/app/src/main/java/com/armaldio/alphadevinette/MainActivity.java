074007a6f5005afb23e0f76772093d67ff58e3d8
Quentin Goinaud <quentin.goinaud@gmail.com>
2026-04-04 12:11:20 +0200

    -

---
+++ android/app/src/main/java/com/armaldio/alphadevinette/MainActivity.java
@@ -0,0 +1,5 @@
+package com.armaldio.alphadevinette;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.armaldio.alphadevinette',
+  appName: 'AlphaDevinette',
+  webDir: 'dist'
+};
+
+export default config;
